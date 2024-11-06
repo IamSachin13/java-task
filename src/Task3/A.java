ca15ff8b76756e1a20f1a687d272f405a9a8c3de
sachin <ravisachin74@gmail.com>
2024-11-06 15:36:02 +0530

    map function

---
+++ src/Task3/A.java
@@ -0,0 +1,9 @@
+package Task3;
+
+echo "# java-task" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/IamSachin13/java-task.git
+git push -u origin main
