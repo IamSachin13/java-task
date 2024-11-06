af640b99f7e5cbde7f3f98cb2228351dd6b3c952
sachin <ravisachin74@gmail.com>
2024-11-06 10:59:05 +0530

    map function

---
+++ src/C1.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class C1 {
+    public static void main(String[] args) {
+        Map<Integer,String> hashMap1=new HashMap<>();
+        //test to chek commit
+    }
+}
